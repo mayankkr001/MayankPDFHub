06c46a8d1e130075fb8de9d8f9d24698cffd90b2
Mayank Kumar <mayankkr00126@gmail.com>
2026-03-24 14:47:36 +0530

    Add files via upload

---
+++ create_png.py
@@ -0,0 +1,4 @@
+import base64, pathlib
+path = pathlib.Path('tmp/test.png')
+data = b'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAoMBgP+iG4cAAAAASUVORK5CYII='
+path.write_bytes(base64.b64decode(data))
